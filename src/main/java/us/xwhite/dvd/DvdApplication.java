8432b81f23d87c24179f252f9d33a73feb227832
Joel Crosswhite <joel.crosswhite@ix.netcom.com>
2017-05-13 07:29:46 -0600

    When cleaning and building the project, was getting compile errors; corrected those by adding additional dependencies to project.

--- src/main/java/us/xwhite/dvd/DvdApplication.java
+++ src/main/java/us/xwhite/dvd/DvdApplication.java
@@ -17,26 +17,11 @@ package us.xwhite.dvd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 public class DvdApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DvdApplication.class, args);
-    }
-}
-
-@Configuration
-@EnableWebSecurity
-class HelloWebSecurityConfiguration extends WebSecurityConfigurerAdapter {
-
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers(HttpMethod.OPTIONS, "/**");
     }
 }
